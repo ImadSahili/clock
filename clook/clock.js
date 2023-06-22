22c3ec2f6e5bdfebbe17a01c896a47b9b63a74cb
imad sahili <123188547+ImadSahili@users.noreply.github.com>
2023-06-22 13:20:24 +0100

    Add files via upload

--- clook/clock.js
+++ clook/clock.js
@@ -2,19 +2,21 @@ const inputValue = window.document.querySelector("input");
 const save = document.querySelector("button");
 const audio = document.querySelector("audio");
 const ul = document.querySelector("ul");
-
+const hTo=document.querySelector("h2")
 save.addEventListener("click", savef);
 let aryTime = [];
 function saveClock() {
   let date = new Date();
   let hh = date.getHours();
   let mm = date.getMinutes();
+  let ss = date.getSeconds()
+  hTo.innerHTML=( hh < 10 ? "0"+hh:hh)+" : "+( mm < 10 ? "0"+mm:mm)+" : "+( ss < 10 ? "0"+ss:ss)
   for (let index = 0; index < aryTime.length; index++) {
-    if (`${"0" + hh}:${mm}` == aryTime[index]) {
+    if (`${hh}:${mm}` == aryTime[index]) {
       aryTime.splice(index, 1);
       audio.play();
       setTimeout(() => {
-        limit();
+        removeJsx();
       }, 30000);
       // setTimeout(() => {
       //   audio.play();
@@ -34,7 +36,7 @@ function savef() {
   ul.innerHTML += `<li>${inputValue.value}</li>`;
 }
 
-function limit() {
+function removeJsx() {
   ul.innerHTML="<ul></ul>"
   for (let i = 0; i < aryTime.length; i++) {
     ul.innerHTML += `<li>${aryTime[i]}</li>`;
